425a7ef73dcb0dfc5efca812950335163ab8e8e7
telamonian <telamonian@users.noreply.github.com>
2020-09-30 08:34:31 -0400

    bugfix for missing cookiecutter vars

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -16,28 +16,28 @@
 
 # if __name__ == "__main__":
 
-#     if not "{{ cookiecutter.has_server_extension }}".lower().startswith("y"):
+#     if not "{{ cookiecutter }}".lower().startswith("y"):
 #         for f in (
-#             "{{ cookiecutter.extension_name }}",
+#             "{{ cookiecutter }}",
 #             "jupyter-config",
 #             "MANIFEST.in",
 #             "pyproject.toml",
 #             "setup.py",
-#             "src/{{ cookiecutter.extension_name }}.ts",
+#             "src/{{ cookiecutter }}.ts",
 #         ):
 #             remove_path(PROJECT_DIRECTORY / f)
 #     else:
-#         if "-" in "{{ cookiecutter.extension_name }}":
+#         if "-" in "{{ cookiecutter }}":
 #             for f in (
-#                 "{{ cookiecutter.extension_name }}",
-#                 "jupyter-config/{{ cookiecutter.extension_name }}.json",
+#                 "{{ cookiecutter }}",
+#                 "jupyter-config/{{ cookiecutter }}.json",
 #             ):
 #                 absolute_f = PROJECT_DIRECTORY / f
 #                 absolute_f.rename(absolute_f.parent / absolute_f.name.replace("-", "_"))
-#         if "_" in "{{ cookiecutter.extension_name }}":
-#             for f in ("src/{{ cookiecutter.extension_name }}.ts", ):
+#         if "_" in "{{ cookiecutter }}":
+#             for f in ("src/{{ cookiecutter }}.ts", ):
 #                 absolute_f = PROJECT_DIRECTORY / f
 #                 absolute_f.rename(absolute_f.parent / absolute_f.name.replace("_", ""))
 
-#     if not "{{ cookiecutter.has_binder }}".lower().startswith("y"):
+#     if not "{{ cookiecutter }}".lower().startswith("y"):
 #         remove_path(PROJECT_DIRECTORY / "binder")

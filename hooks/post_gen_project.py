2516ac5966714a9ed527fa37e9807bbe063c418f
Piyush Jain <pijain@amazon.com>
2022-06-07 16:11:40 -0700

    Updated config and docs

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,43 +1,72 @@
-# #!/usr/bin/env python
-# import os
-# from pathlib import Path
+#!/usr/bin/env python
+import json
+import os
+import shutil
+import sys
+from pathlib import Path
 
-# PROJECT_DIRECTORY = Path.cwd()
+PROJECT_DIRECTORY = Path.cwd()
+
+def update_package_json():
+    try:
+        pkg_config = {}
+        pkg_config_path = PROJECT_DIRECTORY.parent / 'package.json'
+        
+        with open(pkg_config_path, 'r', encoding='utf-8') as f:
+            pkg_config = json.load(f)
+        
+        # Add webpack config
+        jlab_config = pkg_config.get("jupyterlab")
+        if "webpackConfig" not in jlab_config:
+            jlab_config["webpackConfig"] = "webpack.config.js"
+            pkg_config["jupyterlab"] = jlab_config
+
+        # Add source-map-loader
+        dev_dependencies = pkg_config.get("devDependencies")
+        if "source-map-loader" not in dev_dependencies:
+            dev_dependencies["source-map-loader"] = "^3.0.1"
+            pkg_config["devDependencies"] = dev_dependencies
+        
+        with open(pkg_config_path, 'w') as f:
+            json.dump(pkg_config, f, indent=2)
+    except KeyError as e:
+        raise ValueError("package.json doesn't have the jupyterlab configuration.")
 
 
-# def remove_path(path):
-#     if path.is_file():
-#         path.unlink()
-#     elif path.is_dir():
-#         for f in path.iterdir():
-#             remove_path(f)
-#         path.rmdir()
+def update_tsconfig():
+    tsconfig_path = PROJECT_DIRECTORY.parent / 'tsconfig.json'
+    tsconfig = {}
+    
+    with open(tsconfig_path, 'r', encoding='utf-8') as f:
+        tsconfig = json.load(f)
+
+    # make sure sourceMap is true
+    compiler_options = tsconfig.get("compilerOptions")    
+    compiler_options["sourceMap"] = True
+    tsconfig["compilerOptions"] = compiler_options
+
+    with open(tsconfig_path, 'w') as f:
+        json.dump(tsconfig, f, indent=2)
 
 
-# if __name__ == "__main__":
+def create_notebooks_dir():
+    notebooks_dir = "{{cookiecutter.notebooks_dir}}"
+    if not Path(notebooks_dir).is_absolute():
+        notebooks_dir = PROJECT_DIRECTORY.parent / notebooks_dir
 
-#     if not "{{ cookiecutter }}".lower().startswith("y"):
-#         for f in (
-#             "{{ cookiecutter }}",
-#             "jupyter-config",
-#             "MANIFEST.in",
-#             "pyproject.toml",
-#             "setup.py",
-#             "src/{{ cookiecutter }}.ts",
-#         ):
-#             remove_path(PROJECT_DIRECTORY / f)
-#     else:
-#         if "-" in "{{ cookiecutter }}":
-#             for f in (
-#                 "{{ cookiecutter }}",
-#                 "jupyter-config/{{ cookiecutter }}.json",
-#             ):
-#                 absolute_f = PROJECT_DIRECTORY / f
-#                 absolute_f.rename(absolute_f.parent / absolute_f.name.replace("-", "_"))
-#         if "_" in "{{ cookiecutter }}":
-#             for f in ("src/{{ cookiecutter }}.ts", ):
-#                 absolute_f = PROJECT_DIRECTORY / f
-#                 absolute_f.rename(absolute_f.parent / absolute_f.name.replace("_", ""))
+    if not os.path.isdir(notebooks_dir):
+        os.makedirs(notebooks_dir)
 
-#     if not "{{ cookiecutter }}".lower().startswith("y"):
-#         remove_path(PROJECT_DIRECTORY / "binder")
+def copy_webpack_config():
+    shutil.move(
+        os.path.join(str(PROJECT_DIRECTORY), "webpack.config.js"), 
+        os.path.join(str(PROJECT_DIRECTORY.parent), "webpack.config.js")
+    )
+
+
+if __name__ == "__main__":
+    update_package_json()
+    update_tsconfig()
+    copy_webpack_config()
+    create_notebooks_dir()
+
